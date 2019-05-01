22038da43d74b1c213a0fd36a4d02d6f54ff5cc1
Carlos Arcenas <carlos.arcenas@go-jek.com>
2019-05-01 14:42:17 +0700

    Create base tab bar controller and setting controller

---
+++ LISA/SettingsRootTableViewController.swift
@@ -0,0 +1,79 @@
+//
+//  SettingsRootTableViewController.swift
+//  LISA
+//
+//  Created by Carlos Arcenas on 5/1/19.
+//  Copyright © 2019 Carlos Arcenas. All rights reserved.
+//
+
+import UIKit
+
+class SettingsRootTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // Sample table view cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Profile, Payment Methods, Lisa Support
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return 1
+        case 2:
+            return 4
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Profile"
+        case 1:
+            return "Payment Methods"
+        case 2:
+            return "LISA Support"
+        default:
+            return ""
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
